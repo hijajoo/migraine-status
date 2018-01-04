596bb3c44e3ebe1364cdd77857b73213500c96ad
Hina <jajoo.hina03@gmail.com>
2018-01-04 14:07:43 +0530

    Adding testcases for All entries page

--- test/base-tests.js
+++ test/base-tests.js
@@ -14,7 +14,7 @@ describe("Add and display Migraine Status per Day", function(){
     var saveRes;
     var saveBody;
     var linksArr= [];
-    it("should save values of res and body in a variable", function(done){
+    it("should save values of result and body in a variable", function(done){
       request.get(server_url, function(err, res, body){
         saveRes = res;
         saveBody = body;
@@ -29,8 +29,12 @@ describe("Add and display Migraine Status per Day", function(){
     it("should have title Migraine Entry Home", function(){
       expect(saveBody).to.include('<title>Migraine Entry Home</title>');
     });
+    it("should have valid count of entries", function(){
+      var entry_count = saveBody.match(/<li><strong>Migraine Entries:<\/strong> ([0-9]+)<\/li>/);
+      expect(+entry_count[1]).is.ok;
+    })
     it("should contain extractable links", function(){
-      var link_regex = /<a href=\"([a-zA-Z/\-]+)\">/g;
+      var link_regex = /<a href=\"([0-9a-zA-Z/\-]+)\">/g;
       var arr;
       while((arr = link_regex.exec(saveBody)) !== null){
         linksArr.push(server_url + arr[1]);
@@ -55,11 +59,43 @@ describe("Add and display Migraine Status per Day", function(){
         done();
       })
     })
-    it("should have valid count of entries", function(){
-      var entry_count = saveBody.match(/<li><strong>Migraine Entries:<\/strong> ([0-9]+)<\/li>/);
-      expect(+entry_count[1]).is.ok;
-    })
   });
+
+  describe("All entries page", function(){
+    var saveBody;
+    var saveRes;
+    var linksArr = [];
+    it("should save values of result and body in variables", function(done){
+      request(server_url + "/migraine-status/all", function(err, res, body){
+        saveBody = body;
+        saveRes = res;
+        expect(saveBody).is.ok;
+        expect(saveRes).is.ok;
+        done();
+      })
+    });
+    it("should return HTML 200", function(){
+      expect(saveRes.statusCode).to.equal(200);
+    });
+    it("should have title Entries list", function(){
+      expect(saveBody).to.include('<title>Entries List</title>');
+    });
+    it("should contain extractable links", function(){
+      var link_regex = /<a href=\"([0-9a-zA-Z/\-]+)\">/g;
+      var arr;
+      while((arr = link_regex.exec(saveBody)) !== null){
+        linksArr.push(server_url + arr[1]);
+      }
+      expect(linksArr).is.not.empty;
+    });
+    //TDB: This should loop to check all links
+    it("should not contain broken links", function(done){
+      request(linksArr[4], function(err, res, body){
+        expect(res.statusCode).to.equal(200);
+        done();
+      })
+    });
+  })
 
   after(function(){
     server.close();
